64d4ead809dd19ab20b94a38dfcfb8f44486bf30
headwindtrend <128255279+headwindtrend@users.noreply.github.com>
2023-03-29 13:16:14 +0800

    Update my_panel.py
    Replaced all "MyPanelCommand." with "self."

--- my_panel.py
+++ my_panel.py
@@ -19,10 +19,10 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 
 	def run(self, text=None):
 		# Load from history upfront
-		if MyPanelCommand.enum == 0:
-			with open(MyPanelCommand.historyfile, "r") as f:
-				MyPanelCommand.items += [item.strip("\n") for item in f.readlines()]
-		MyPanelCommand.enum += 1
+		if self.enum == 0:
+			with open(self.historyfile, "r") as f:
+				self.items += [item.strip("\n") for item in f.readlines()]
+		self.enum += 1
 		view = self.window.active_view()
 
 		def s_handler(endpoint, skip_input=False):
@@ -39,7 +39,7 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 				self.show_history(text)
 			# Otherwise, bring up input_panel with the selected text
 			else:
-				MyPanelCommand.orisel = list(view.sel())	# Keep the current selection in active_view for text-insert purpose
+				self.orisel = list(view.sel())	# Keep the current selection in active_view for text-insert purpose
 				if skip_input: self.on_done(view.substr(view.sel()[0]))	# Search it directly as this skip_input flag implied
 				else: self.window.show_input_panel("Search:", view.substr(view.sel()[0]), self.on_done, None, self.on_cancel)
 
@@ -51,7 +51,7 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 			# Otherwise, if not this "[=escape=]" particular string, it probably came from the history list, show it on input_panel to enable further adjustment
 			elif text != "[=escape=]":
 				# Provided that it's an existing item
-				if text in MyPanelCommand.items:
+				if text in self.items:
 					self.window.show_input_panel("Search:", text, self.on_done, None, self.on_cancel)
 				# Otherwise, search it directly
 				else: self.window.run_command("hide_overlay"); self.on_done(text)
@@ -59,7 +59,7 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 		else: s_handler(view.line(view.sel()[0].begin()).end(), True)
 
 	def show_history(self, text):
-		items = MyPanelCommand.items
+		items = self.items
 		if len(items) > 0:
 			self.window.show_quick_panel(items, lambda idx: self.pick(idx, items, text))
 		else:
@@ -72,38 +72,38 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 			# Check if any of the keywords for deleting history item is in place
 			if re.search(r"\[del(?:ete)?\]|\[remove\]", text):
 				text = re.sub(r"\[del(?:ete)?\]|\[remove\]", "", text)
-				items = MyPanelCommand.items
+				items = self.items
 				if text in items:
 					items.remove(text)
 					# Save changes
-					with open(MyPanelCommand.historyfile, "w") as f:
-						for item in MyPanelCommand.items: f.write(item + "\n")
+					with open(self.historyfile, "w") as f:
+						for item in self.items: f.write(item + "\n")
 				return self.window.run_command("my_panel")
 			# No extra space by default
-			MyPanelCommand.extraspace = ""
+			self.extraspace = ""
 			# Check if a request is made for adding an extra leading space to the assortment return
 			if re.search(r"^\s*=[ ;]=", text):
-				MyPanelCommand.extraspace += ";" if re.search(r"^\s*=;=", text) else ""
-				MyPanelCommand.extraspace += "head"
+				self.extraspace += ";" if re.search(r"^\s*=;=", text) else ""
+				self.extraspace += "head"
 				text = re.sub(r"^\s*=[ ;]=", "", text)
 			# Check if a request is made for adding an extra trailing space to the assortment return
 			if re.search(r"=[ ;]=\s*$", text):
-				MyPanelCommand.extraspace += ("&" if MyPanelCommand.extraspace[-4:] == "head" else "") + "tail"
-				MyPanelCommand.extraspace += ";" if re.search(r"=;=\s*$", text) else ""
+				self.extraspace += ("&" if self.extraspace[-4:] == "head" else "") + "tail"
+				self.extraspace += ";" if re.search(r"=;=\s*$", text) else ""
 				text = re.sub(r"=[ ;]=\s*$", "", text)
 			# Keep a history of all the entries but those already exist
-			if text not in MyPanelCommand.items:
-				MyPanelCommand.items.insert(0, text)
+			if text not in self.items:
+				self.items.insert(0, text)
 				# Save changes
-				with open(MyPanelCommand.historyfile, "w") as f:
-					for item in MyPanelCommand.items: f.write(item + "\n")
+				with open(self.historyfile, "w") as f:
+					for item in self.items: f.write(item + "\n")
 			# Do the essential process
 			results = self.get_matched_lines(self.do_transformation(text))
 			if results == [">>>Timeout<<<"]: self.prompt_timeout(view); return
-			# print(MyPanelCommand.tmbtp_itself)#debug
+			# print(self.tmbtp_itself)#debug
 			# Show the matched lines in quick_panel if found anything
-			if len(results) > 0 and not MyPanelCommand.tmbtp_itself:
-				view.erase_regions("MyPanel"); view.add_regions("MyPanel", view.find_all(MyPanelCommand.mark, sublime.IGNORECASE if MyPanelCommand.case_i else 0), "string", "dot")
+			if len(results) > 0 and not self.tmbtp_itself:
+				view.erase_regions("MyPanel"); view.add_regions("MyPanel", view.find_all(self.mark, sublime.IGNORECASE if self.case_i else 0), "string", "dot")
 				self.window.show_quick_panel(results, lambda idx: self.mpick(idx, results, text), 1, 0, lambda idx: self.on_highlight(idx, results))
 			# Otherwise, attempt shorthand search
 			else:
@@ -111,7 +111,7 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 				if results == [">>>Timeout<<<"]: self.prompt_timeout(view); return
 				# Show the matched lines in quick_panel if found anything
 				if len(results) > 0:
-					view.erase_regions("MyPanel"); view.add_regions("MyPanel", view.find_all(MyPanelCommand.mark, sublime.IGNORECASE if MyPanelCommand.case_i else 0), "string", "dot")
+					view.erase_regions("MyPanel"); view.add_regions("MyPanel", view.find_all(self.mark, sublime.IGNORECASE if self.case_i else 0), "string", "dot")
 					self.window.show_quick_panel(results, lambda idx: self.mpick(idx, results, text), 1, 0, lambda idx: self.on_highlight(idx, results))
 				else:
 					print("Nothing matched!"); self.window.status_message("Nothing matched!")
@@ -119,7 +119,7 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 					view.show_popup("<b style='background-color:red;color:lime'>: Nothing_matched! :</b>")
 		# Otherwise, show the history in quick_panel
 		else:
-			items = MyPanelCommand.items
+			items = self.items
 			if len(items) > 0:
 				self.show_history(text)
 			else:
@@ -137,16 +137,16 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 	# Do the transformation if the syntax is matched
 	def do_transformation(self, text, option=""):
 		# Case insensitive by default
-		MyPanelCommand.case_i = True
+		self.case_i = True
 		# Handle case sensitive declaration if specified
 		if re.search(r"^\s*=cs=|=cs=\s*$", text):
-			MyPanelCommand.case_i = False
+			self.case_i = False
 			text = re.sub(r"^\s*=cs=|=cs=\s*$", "", text)
 		# Maintain chronological order for assortment by default
-		MyPanelCommand.ass_ao = False
+		self.ass_ao = False
 		# Handle assortment ascending order request
 		if re.search(r"^\s*=aa=|=aa=\s*$", text):
-			MyPanelCommand.ass_ao = True
+			self.ass_ao = True
 			text = re.sub(r"^\s*=aa=|=aa=\s*$", "", text)
 		# Prepare the text for shorthand search
 		if option == "shorthand":
@@ -158,7 +158,7 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 			text = re.sub(r"//$", "", re.sub(r"\s+", " ", text.strip()))
 			ta = text.strip().split("||") if re.search(r"^.+\|\|.+", text.strip()) else [text.strip()]
 			text = ""
-			MyPanelCommand.mark = ""
+			self.mark = ""
 			for i in range(len(ta)):
 				ts = ta[i].split(" ")
 				text += "^"
@@ -167,17 +167,17 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 						text += "(?!.*(?:" + ts[j][1:] + "))"
 					else:
 						text += "(?=.*(?:" + ts[j] + "))"
-						MyPanelCommand.mark += "|" + ts[j]
+						self.mark += "|" + ts[j]
 				text += ".*$"
 			text = text.replace(".*$^(?", ".*$|^(?")
-			MyPanelCommand.mark = MyPanelCommand.mark[1:]
+			self.mark = self.mark[1:]
 		elif re.search(r"^/.+/$", text.strip()):
 			text = text.strip()[1:-1]
-			MyPanelCommand.mark = text
+			self.mark = text
 		else:
 			if "\\w{0,3}" not in text:
 				text = re.escape(text); text = text.replace(r"\`", "`")
-			MyPanelCommand.mark = text
+			self.mark = text
 		sublime.set_clipboard(text)	# This exists for the user convenience as s/he may want to use the pattern to find the "needle" by other means, for instance, by ctrl+f
 		return text
 
@@ -187,14 +187,14 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 		assortm = []
 		seen = []
 		lastfound = -1
-		timeout = time.time() + MyPanelCommand.maxtol	# default to 5 seconds from now
+		timeout = time.time() + self.maxtol	# default to 5 seconds from now
 		view = self.window.active_view()
 		# Prevent those assorted matches, being deemed "too many", from entering the main loop (as it unnecessarily slowdowns the process)
-		slimark = MyPanelCommand.mark
+		slimark = self.mark
 		if "|" in slimark and "\\w{0,3}" not in slimark:
 			ma = slimark.split("|")
 			for i in range(len(ma)):
-				n = len(view.find_all(ma[i], sublime.IGNORECASE if MyPanelCommand.case_i else 0))
+				n = len(view.find_all(ma[i], sublime.IGNORECASE if self.case_i else 0))
 				if n > 99:	# definition of "too many"
 					assortm += [str(n) + " <<< " + ma[i]]
 					slimark = ("|" + slimark + "|").replace("|" + ma[i] + "|", "|"); slimark = slimark[1:-1]
@@ -203,12 +203,12 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 		# Prepare for the line number formatting
 		row, _ = view.rowcol(view.size())
 		line_count = row + 1
-		MyPanelCommand.lc_len = len(str(line_count))
-		format_str = "{:>" + str(MyPanelCommand.lc_len) + "}:"
+		self.lc_len = len(str(line_count))
+		format_str = "{:>" + str(self.lc_len) + "}:"
 		# Find all possible marks as the preparation for assortment
-# 		marks = [(view.substr(i), i.begin(), i.end()) for i in view.find_all(slimark if slimark else MyPanelCommand.mark, sublime.IGNORECASE if MyPanelCommand.case_i else 0)]
+# 		marks = [(view.substr(i), i.begin(), i.end()) for i in view.find_all(slimark if slimark else self.mark, sublime.IGNORECASE if self.case_i else 0)]
 		# Find all regions that match the text
-		regions = view.find_all(text, sublime.IGNORECASE if MyPanelCommand.case_i else 0)
+		regions = view.find_all(text, sublime.IGNORECASE if self.case_i else 0)
 		# Loop through each region
 		for ri, region in enumerate(regions):
 			# Get the line number of the region
@@ -219,27 +219,27 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 				# Append the result to the list
 				result = format_str.format(line_number + 1) + " " + line_text
 				results.append(result)
-# 				assortm += [i[0] for i in marks if i[1] >= view.line(region).begin() and i[2] <= view.line(region).end()]	#re.findall(MyPanelCommand.mark, line_text, re.IGNORECASE if MyPanelCommand.case_i else 0)
-				assortm += re.findall(slimark if slimark else MyPanelCommand.mark, line_text, re.IGNORECASE if MyPanelCommand.case_i else 0)
+# 				assortm += [i[0] for i in marks if i[1] >= view.line(region).begin() and i[2] <= view.line(region).end()]	#re.findall(self.mark, line_text, re.IGNORECASE if self.case_i else 0)
+				assortm += re.findall(slimark if slimark else self.mark, line_text, re.IGNORECASE if self.case_i else 0)
 			lastfound = line_number
 			if time.time() > timeout:
 				percent_completed = (ri + 1) / len(regions) * 100
 				themessage = str(int(percent_completed)) + "% completed\n\nDo you want to continue?"
 				yesno = sublime.yes_no_cancel_dialog(themessage, "Yes", "No")
-				if yesno == sublime.DIALOG_YES: timeout = time.time() + MyPanelCommand.maxtol	# reset timer
+				if yesno == sublime.DIALOG_YES: timeout = time.time() + self.maxtol	# reset timer
 				else: return [">>>Timeout<<<"]
-		# print(MyPanelCommand.mark)#debug
+		# print(self.mark)#debug
 		# print(assortm)#debug
 		stass = [item.strip() for item in assortm]
-		if MyPanelCommand.ass_ao:	# Ascending order
+		if self.ass_ao:	# Ascending order
 			ulist = sorted([(i, stass.count(i)) for i in set(stass)], key=lambda x: x[0]) #; print(ulist)#debug
 		else:	# Chronological order
 			ulist = [(i, stass.count(i)) for i in [i for i in stass if i not in seen and not seen.append(i)]] #; print(ulist)#debug
-		MyPanelCommand.mc_len = len(str(max(ulist, key=lambda x: x[1])[1])) if len(ulist) > 0 else 0
-		assortm = [("{:>" + str(MyPanelCommand.mc_len) + "} " + (">>>" if "<<<" in key else "<<<") + " {}").format(value, key) for key, value in ulist]
+		self.mc_len = len(str(max(ulist, key=lambda x: x[1])[1])) if len(ulist) > 0 else 0
+		assortm = [("{:>" + str(self.mc_len) + "} " + (">>>" if "<<<" in key else "<<<") + " {}").format(value, key) for key, value in ulist]
 		# print(assortm)#debug
-		MyPanelCommand.tmbtp_itself = (len(regions) == 1 and regions[0].begin() in range(view.sel()[0].begin(), view.sel()[0].end()))
-		# print(MyPanelCommand.tmbtp_itself)#debug
+		self.tmbtp_itself = (len(regions) == 1 and regions[0].begin() in range(view.sel()[0].begin(), view.sel()[0].end()))
+		# print(self.tmbtp_itself)#debug
 		return assortm + results
 
 	# A helper function that runs this command again with the selected item
@@ -259,22 +259,22 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 		if index >= 0:
 			# If one of the assorted matches is picked, insert it directly
 			if re.search(r"^(?:\s*\d+ >>> )?\s*\d+ <<< ", results[index]):
-				v = results[index].find(" <<< ") + 5	#MyPanelCommand.mc_len + 5
-				view.sel().clear(); view.sel().add_all(MyPanelCommand.orisel)	# Resume the original selection in active_view beforehand
-				head = " " if MyPanelCommand.extraspace.startswith("head") else ";" if MyPanelCommand.extraspace.startswith(";") else ""
-				tail = " " if MyPanelCommand.extraspace.endswith("tail") else ";" if MyPanelCommand.extraspace.endswith(";") else ""
+				v = results[index].find(" <<< ") + 5	#self.mc_len + 5
+				view.sel().clear(); view.sel().add_all(self.orisel)	# Resume the original selection in active_view beforehand
+				head = " " if self.extraspace.startswith("head") else ";" if self.extraspace.startswith(";") else ""
+				tail = " " if self.extraspace.endswith("tail") else ";" if self.extraspace.endswith(";") else ""
 				view.run_command("insert", {"characters": head + results[index][v:] + tail})
-				MyPanelCommand.orisel = list(view.sel())	# Adopt new position(s) in active_view for next text-insert
+				self.orisel = list(view.sel())	# Adopt new position(s) in active_view for next text-insert
 				# Run this command again with the untransformed text
 				self.window.run_command("my_panel", {"text": text})
 			# Otherwise, goto the picked line
 			else:
-				v = MyPanelCommand.lc_len + 2
+				v = self.lc_len + 2
 				# If it's my very log file, remove the first seven characters (which is the date stamp of that line) and insert it directly
-				if view.file_name() == MyPanelCommand.filepath + r"\log.txt":
-					view.sel().clear(); view.sel().add_all(MyPanelCommand.orisel)	# Resume the original selection in active_view beforehand
+				if view.file_name() == self.filepath + r"\log.txt":
+					view.sel().clear(); view.sel().add_all(self.orisel)	# Resume the original selection in active_view beforehand
 					view.run_command("insert", {"characters": results[index][v+7:]})
-					MyPanelCommand.orisel = list(view.sel())	# Adopt new position(s) in active_view for next text-insert
+					self.orisel = list(view.sel())	# Adopt new position(s) in active_view for next text-insert
 					# Run this command again with the untransformed text
 					self.window.run_command("my_panel", {"text": text})
 					return
@@ -284,8 +284,8 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 				# Run this command again with the untransformed text
 				self.window.run_command("my_panel", {"text": text})
 		else:
-			head = "= =" if MyPanelCommand.extraspace.startswith("head") else "=;=" if MyPanelCommand.extraspace.startswith(";") else ""
-			tail = "= =" if MyPanelCommand.extraspace.endswith("tail") else "=;=" if MyPanelCommand.extraspace.endswith(";") else ""
+			head = "= =" if self.extraspace.startswith("head") else "=;=" if self.extraspace.startswith(";") else ""
+			tail = "= =" if self.extraspace.endswith("tail") else "=;=" if self.extraspace.endswith(";") else ""
 			# Run this command again with the untransformed text
 			self.window.run_command("my_panel", {"text": head + text + tail})
 
@@ -293,7 +293,7 @@ class MyPanelCommand(sublime_plugin.WindowCommand):
 	def on_highlight(self, index, results):
 		if not re.search(r"^(?:\s*\d+ >>> )?\s*\d+ <<< ", results[index]):
 			view = self.window.active_view()
-			line_number = int(results[index][:MyPanelCommand.lc_len])
+			line_number = int(results[index][:self.lc_len])
 			line_region = view.line(view.text_point(line_number - 1, 0))
 			mid_point = line_region.begin() + (line_region.end() - line_region.begin()) / 2
 			view.sel().clear(); view.sel().add(sublime.Region(mid_point, mid_point))
